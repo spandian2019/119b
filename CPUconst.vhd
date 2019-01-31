6ed92ccaf2f999a22a6402df0df2ea8fb1d65ef2
Sundar Pandian <spandian2019@gmail.com>
2019-01-30 23:16:47 -0800

    Added constants file

---
+++ CPUconst.vhd
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------------
+--
+--  Control Unit constants package
+--
+--  This package defines control unit constants for supported AVR
+--  instructions
+--
+--  Revision History
+--      1/30/19   Sundar Pandian    initial revision
+--
+-----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package constants is
+
+    subtype  ALU_selects  is  std_logic_vector(2 downto 0);
+
+--  ALU select opcode constants
+    
+    constant AddSubEn       : ALU_selects := "000";
+    constant FBlockEn       : ALU_selects := "001";
+    constant ShiftEn        : ALU_selects := "010";
+
+
+end package;
